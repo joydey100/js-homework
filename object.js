77f407712c68af7f8889172fcfb7ce2661b94aab
Joy Dey <joydey.bd.2002@gmail.com>
2021-08-31 23:24:31 +0600

    update

--- object.js
+++ object.js
@@ -1,13 +1,24 @@
 const obj = {
   name: "karim",
   age: 25,
+  money: 5000,
+
+  treat: function (expense) {
+    console.log(this.money - expense);
+  },
 };
 
-// console.log(Object.keys(obj));
-// console.log(Object.values(obj));
+const another = {
+  name: "abdul",
+  age: 20,
+  money: 1000,
+};
 
-let keys = Object.keys(obj);
+let anotherTreat = obj.treat.bind(another);
+anotherTreat(100);
 
-for (let key of keys) {
-  console.log(obj[key]);
-}
+// anotherTreat(100);
+
+// obj.treat.call(another, 400);
+
+// obj.treat.apply(another, [100]);
